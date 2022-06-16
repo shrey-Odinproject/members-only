cd7065242992fc447cdaa0497650c8df28f65d78
shrey-Odinproject <sonishreyf1@gmail.com>
2022-06-16 13:03:40 +0530

    add relation between posts and users using user_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates :body, presence: true, length: { in: 6..20 }
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_061932) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_073135) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
