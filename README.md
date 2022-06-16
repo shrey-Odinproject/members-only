2ad1a1743f342eb244e63ef70e10000f34d428a5
shrey-Odinproject <sonishreyf1@gmail.com>
2022-06-16 15:05:49 +0530

    Fix issue of not being redireted to index after user sign up
    used a method

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # README
 
 rails app with authorization
+email poster1@mail (2,3..)
+password 123456
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
+  def after_sign_up_path_for(user)
+    '/'
+  end
   private
   def sign_up_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
