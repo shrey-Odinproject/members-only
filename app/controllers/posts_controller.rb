ee0eb913e67de7f2895c4fa0d31f62dab8c1b45e
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-16 18:46:40 +0530

    Allow pic to be passed in strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body, :pic)
   end
 
 end
